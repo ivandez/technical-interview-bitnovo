eef06ce1e3009f878319374cc9bf92925909080a
ivandez <ivan.mendez.dev@gmail.com>
2024-01-18 12:06:54 -0400

    center footer

--- src/components/global/Layout.tsx
+++ src/components/global/Layout.tsx
@@ -4,13 +4,15 @@ const Layout = ({ children }: any) => {
   return (
     <div className="grid h-screen" style={{ gridTemplateRows: "90% 10%" }}>
       <div className="flex items-center justify-center w-full">{children}</div>
-      <div>
-        <Image
-          src="/footer-content.jpg"
-          alt="footer content"
-          width={500}
-          height={500}
-        />
+      <div className="flex justify-center items-end px-4 pb-3">
+        <footer>
+          <Image
+            src="/footer-content.jpg"
+            alt="footer content"
+            width={500}
+            height={500}
+          />
+        </footer>
       </div>
     </div>
   );
